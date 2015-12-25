51a74639c002f4dfe8746dda9f4cc792210cebed
Ahmed Hassan <a.hassan@buismed.de>
2015-12-25 17:09:40 +0100

    inital commit

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+    name: 'flowkey:hls-js',
+    version: '0.4.0',
+
+    // Brief, one-line summary of the package.
+    summary: 'HLS.js - Javascript only HLS implementation',
+
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/dailymotion/hls.js',
+
+    // To avoid submitting documentation, set this field to null.
+    documentation: null
+});
+
+Package.onUse(function(api) {
+    api.addFiles('hls-js.js');
+});
